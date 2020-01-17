664fdaf46af2c8be7e5ba0d6670fc68560916cda
Kevin Schoell <schoell411@gmail.com>
2020-01-17 03:56:04 -0600

    tests working

--- testing-app2/src/App.test.js
+++ testing-app2/src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
+test("displays balls", () => {
+  const { getAllByText } = render(<App />);
+  getAllByText(/balls/i);
+});
+
+test("displays strikes", () => {
+  const { getAllByText } = render(<App />);
+  getAllByText(/strikes/i);
+});
+
+test("displays hit", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  getByText(/hit/i);
+});
+
+test("displays fouls", () => {
+  const { getByText } = render(<App />);
+  getByText(/foul/i);
+});
+
+test("it renders without crashing", () => {
+  render(<App />);
 });
